a7950a3141eb6f4d96381bc41b5382a56bbfe9e6
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2021-10-08 09:54:14 +0200

    Add bootstrap to make table responsive

--- publish.py
+++ publish.py
@@ -5,6 +5,9 @@ SHEET_URL = "https://docs.google.com/spreadsheets/d/1DqCJy4qOGNR4UD-PLHOQwyByDBl
 
 df = pd.read_csv(SHEET_URL)
 df = df.drop(['Timestamp', 'Contact email'], axis=1)
+df = df.reindex(columns=['Core facility name', 'Services', 'Equipment', 'Expertise','Website', 'Address',
+       'Organization', 'Access model'])
+print(df.columns)
 df.Website = df.Website.apply(lambda url: f'<a href="{url}">{url}</a>')
 # df['Contact email'] = df['Contact email'].apply(lambda email: f'<a href="mailto:{email}">{email}</a>')
 
@@ -19,6 +22,7 @@ template = jinja2.Template("""<!DOCTYPE html>
     <title>Demo</title>
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" type="text/css">
     <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" type="text/javascript"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
     <link rel="stylesheet" href="./style.css">
     <link rel="stylesheet" href="./table-style.css">
     </head>
@@ -29,7 +33,9 @@ template = jinja2.Template("""<!DOCTYPE html>
         Please contact Nikolaus Sonnenschein (<a href="mailto:niso@dtu.dk">niso@dtu.dk</a>) or Magali Michaut (<a href="mailto:magali.michaut@sund.ku.dk">magali.michaut@sund.ku.dk</a>) if you are leading a core facility
         in Denmark and would like to add it to this list. For inquiries to the respective facilities, please contact them directly by using the contact information provided on their websites.</p>
 
-        {{ dataframe }}
+        <div class="table-responsive">
+            {{ dataframe }}
+        </div>
 
     </body>
 
@@ -40,7 +46,7 @@ template = jinja2.Template("""<!DOCTYPE html>
 </html>"""
 )
 
-output_html = template.render(dataframe=df.to_html(table_id="myTable", index=False, escape=False, border = 0))
+output_html = template.render(dataframe=df.to_html(table_id="myTable", index=False, escape=False, border = 0, classes='table table-striped'))
 
 # Write generated HTML to file.
 with open("build/index.html", "w", encoding="utf-8") as file_obj:
