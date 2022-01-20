55b9c629ef758a893b5a5077090026a0711f3cb2
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2022-01-20 10:02:49 +0100

    Refactor HTML tempalte into separate file

--- publish.py
+++ publish.py
@@ -10,39 +10,10 @@ df = df.reindex(columns=['Core facility name', 'Services', 'Equipment', 'Experti
 df.Website = df.Website.apply(lambda url: f'<a href="{url}">{url}</a>')
 # df['Contact email'] = df['Contact email'].apply(lambda email: f'<a href="mailto:{email}">{email}</a>')
 
-
+with open('template.html') as f:
+    raw_template = f.read()
 # Generate HTML from template.
-template = jinja2.Template("""<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width">
-    <title>Demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" type="text/css">
-    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" type="text/javascript"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
-    <link rel="stylesheet" href="./style.css">
-    </head>
-    <h1>🇩🇰 Danish Life Science Research Core Facilities 👩‍🔬 🧬 🔬 🧪 📊 🦾 🦠 🧫 👨‍🔬</h1>
-    <body>
-
-        <p>This websites lists Danish Life Science Research core facilities in an easy to search format.
-        Please contact Nikolaus Sonnenschein (<a href="mailto:niso@dtu.dk">niso@dtu.dk</a>) or Magali Michaut (<a href="mailto:magali.michaut@sund.ku.dk">magali.michaut@sund.ku.dk</a>) if you are leading a core facility
-        in Denmark and would like to add it to this list. For inquiries to the respective facilities, please contact them directly by using the contact information provided on their websites.</p>
-
-        <div class="table-responsive">
-            {{ dataframe }}
-        </div>
-
-    </body>
-
-    <script defer type="text/javascript">
-        let myTable = new simpleDatatables.DataTable("#myTable");
-    </script>
-
-</html>"""
-)
+template = jinja2.Template(raw_template)
 
 output_html = template.render(dataframe=df.to_html(table_id="myTable", index=False, escape=False, border = 0, classes='table table-striped'))
 
